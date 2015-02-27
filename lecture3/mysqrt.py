dfdfc7962c4bc169a6bc87f96856ceff5bed355d
Io Astorga <patitoalrves@gmail.com>
2015-02-26 16:36:54 -0800

    Check convergence

--- lecture3/mysqrt.py
+++ lecture3/mysqrt.py
@@ -1,9 +1,15 @@
 
 from numpy import *
 
-x=9.
+x=100.
 s=1.
-for k in range(7):
+kmax = 100
+tol = 1.e-14
+for k in range(kmax):
     print "Before iteration %s, s = %s" % (k,s) 
+    s0 = s
     s=0.5*(s+x/s)
+    delta_s = s - s0
+    if abs(delta_s / x) < tol:
+        break
 print "After %s iterations, s= %s" % (k+1,s)
