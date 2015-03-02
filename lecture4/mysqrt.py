4619e200128a0b486be97b416dec3444fbb40dc8
Io Astorga <patitoalreves@gmail.com>
2015-03-02 15:28:14 -0800

    added debug flag

--- lecture4/mysqrt.py
+++ lecture4/mysqrt.py
@@ -1,13 +1,23 @@
+"""
+Some details here
+"""
 
-def sqrt2(x):
+def sqrt2(x, debug=False):
+    """
+    by default debug is false
+    more details
+    """
     s=1.
     kmax = 100
     tol = 1.e-14
     for k in range(kmax):
-        print "Before iteration %s, s = %s" % (k,s) 
+        if debug:
+            print "Before iteration %s, s = %s" % (k,s) 
         s0 = s
         s=0.5*(s+x/s)
         delta_s = s - s0
         if abs(delta_s / x) < tol:
             break
-    print "After %s iterations, s= %s" % (k+1,s)
+    if debug:
+        print "After %s iterations, s= %s" % (k+1,s)
+    return s
